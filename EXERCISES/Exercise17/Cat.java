9653b5e373c9f1659e990a7bebc774b64b71e9ab
gwen041 <almiratabios01@gmail.com>
2026-01-09 16:01:54 +0800

    Add: set and age checker method in cat class

--- EXERCISES/Exercise17/Cat.java
+++ EXERCISES/Exercise17/Cat.java
@@ -2,7 +2,9 @@ package EXERCISES.Exercise17;
 
 public class Cat {
     private String name;
+    private String breed;
     private int age;
+    
 
     public Cat(String name, int age) {
         this.name = name;
@@ -17,4 +19,23 @@ public class Cat {
         return age;
     }
     
+    public void setBreed(String breed) {
+        this.breed = breed;
+    }
+
+    public String getBreed() {
+        return breed;
+    }
+
+    public boolean ageChecker() {
+        if (this.age > 1) {
+            return false;
+        }
+        return true;
+    }
 }
+
+    // Write a Java program where the "Cat" class includes a method to set a cat's breed.
+    // Write a Java program where the "Cat" class supports a method to determine if a cat is a kitten (age < 1 year).
+    // Write a Java program where the "Cat" class allows setting and getting the cat’s weight.
+    // Write a Java program where the "Cat" class includes a method to compare the ages of two cats.
--- EXERCISES/Exercise17/Main.java
+++ EXERCISES/Exercise17/Main.java
@@ -10,15 +10,21 @@ public class Main {
         System.out.print("Enter the cat's name: ");
         String catName = in.nextLine();
 
+        System.out.print("Enter the cat's breed: ");
+        String catBreed = in.nextLine();
+
         System.out.print("Enter the cat's age: ");
         int catAge = in.nextInt();
+        
 
         in.close();
 
         Cat cat1 = new Cat(catName, catAge);
+        cat1.setBreed(catBreed);
 
-        System.out.println("The name of the cat is " + cat1.getName());
+        System.out.println("\nThe name of the cat is " + cat1.getName());
         System.out.println("The age of the cat is " + cat1.getAge());
-    }
-    
+        System.out.println("The breed of the cat is " + cat1.getBreed());
+        System.out.println("The cat is a kitten: " + cat1.ageChecker());
+    }   
 }
