a629dd04cea1f2e1cc6d06d1580e8c33aaa3c740
gwen041 <almiratabios01@gmail.com>
2026-03-24 15:33:17 +0800

    testing

---
+++ ARRAYS/Array14/jes.java
@@ -0,0 +1,28 @@
+package ARRAYS.Array14;
+
+import java.util.Scanner;
+//wala pa kasing jdk yung baby ko
+
+public class jes {
+  public static void main(String[] args) {
+    myFunction();
+  }
+
+  public static void myFunction() {
+    Scanner in = new Scanner(System.in);
+    System.out.print("\nEnter length: ");
+    int length = in.nextInt();  
+
+    int[] arr = new int[length];
+
+    for(int i = 0; i < arr.length; i++) {
+      arr[i] = i+1;
+      System.out.println(printArray(arr[i]));
+    }
+    in.close();
+  }
+
+  public static int printArray(int num) {
+    return num;
+  }
+}
